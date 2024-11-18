3ff37a29d330184929dd353d254afb519e28d2a8
KiritoEM <johankirito64@gmail.com>
2024-11-18 12:52:41 +0300

    feat: add register to /api

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,10 +1,34 @@
 import { prisma } from "@/lib/prisma";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    console.log(req.body);
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
+
+    const user = await prisma.user.create({
+      data: {
+        email: "",
+        password: "",
+        pseudo: "",
+      },
+    });
+
+    if (user) {
+      return NextResponse.json(
+        { message: "account created successfully" },
+        { status: 200 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "account created successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
-};
-
-export default handler;
+}
